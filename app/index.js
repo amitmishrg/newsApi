5996c096763b4af47111c8aee3515fb4a6100f93
Amit Mishra <amituidev>
2018-12-19 11:58:45 +0530

    cors fixed

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { errorHandler } = require('./utils/error-handler');
+const cors = require('cors');
 
 module.exports = (app, config) => {
+  app.use(cors());
   require('./routes')(app, config);
   app.use(errorHandler)
 }
